441abd5af2be3d4a8f0ce4afebe01415da8613ac
Zhan Cao <ohyesboy@gmail.com>
2020-05-24 00:41:21 -0400

    include jquery bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+    $(document).ready(function(){
+      $('a[href^="#"]').on('click',function (e) {e.preventDefault();
+
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+  });
+  }
   title = 'InterestCalc';
 }
