22333ef13aa993c165bf70377fa88d2df18ae476
OhyesboyFromDell <ohyesboy@gmail.com>
2021-01-10 00:11:47 -0500

    Fix bug

--- build-docker-run.bat
+++ build-docker-run.bat
@@ -1,4 +1,5 @@
 docker rm -f calc1
-call build-InterestCalc.bat
-docker build -f DockerfileBuild dist -t calc
+REM do not need to build locally because of 2 stage build
+REM call build-InterestCalc.bat
+docker build -f DockerfileBuild . -t calc
 docker run -d -p 8080:80 --name calc1 calc
